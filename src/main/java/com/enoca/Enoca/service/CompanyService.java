b93d6fe578b7ba7a68b0ba6ed3b536f28a53cd3f
Mehmet Alpkaya <alpkaya71@gmail.com>
2023-07-21 23:59:24 +0300

    CRUD added commit

--- src/main/java/com/enoca/Enoca/service/CompanyService.java
+++ src/main/java/com/enoca/Enoca/service/CompanyService.java
@@ -1,8 +1,11 @@
 package com.enoca.Enoca.service;
 
+import com.enoca.Enoca.entity.Company;
 import com.enoca.Enoca.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CompanyService
@@ -12,6 +15,31 @@ public class CompanyService
     @Autowired
     public CompanyService(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
+    }
+    // Şirket Ekleme
+    public Company addCompany(Company company) {
+        return companyRepository.save(company);
+    }
+
+    // Şirket Güncelleme
+    public Company updateCompany(Long companyId, Company updatedCompany) {
+        Company company = companyRepository.findById(companyId)
+                .orElseThrow(() -> new IllegalArgumentException("Şirket bulunamadı: " + companyId));
+
+        company.setName(updatedCompany.getName());
+        // diğer alanlar güncellenirse buraya ekleyin
+
+        return companyRepository.save(company);
+    }
+
+    // Şirket Silme
+    public void deleteCompany(Long companyId) {
+        companyRepository.deleteById(companyId);
+    }
+
+    // Tüm Şirketleri Listeleme
+    public List<Company> getAllCompanies() {
+        return companyRepository.findAll();
     }
 
 }
--- src/main/java/com/enoca/Enoca/service/EmployeeService.java
+++ src/main/java/com/enoca/Enoca/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.enoca.Enoca.service;
 
+import com.enoca.Enoca.entity.Employee;
 import com.enoca.Enoca.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EmployeeService {
@@ -11,5 +14,32 @@ public class EmployeeService {
     @Autowired
     public EmployeeService(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
+    }
+
+    // Çalışan Ekleme
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    // Çalışan Güncelleme
+    public Employee updateEmployee(Long employeeId, Employee updatedEmployee) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new IllegalArgumentException("Çalışan bulunamadı: " + employeeId));
+
+        employee.setName(updatedEmployee.getName());
+        employee.setPosition(updatedEmployee.getPosition());
+        // diğer alanlar güncellenirse buraya ekleyin
+
+        return employeeRepository.save(employee);
+    }
+
+    // Çalışan Silme
+    public void deleteEmployee(Long employeeId) {
+        employeeRepository.deleteById(employeeId);
+    }
+
+    // Tüm Çalışanları Listeleme
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
     }
 }
