af660b96b67826c1dfde432a0804f401995b8aae
Mehmet Alpkaya <alpkaya71@gmail.com>
2023-07-22 00:07:09 +0300

    last commit

---
+++ src/main/java/com/enoca/Enoca/configuration/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.enoca.Enoca.configuration;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("your.package.name.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(" API ")
+                .description("Description")
+                .version("1.0.0")
+                .build();
+    }
+}
